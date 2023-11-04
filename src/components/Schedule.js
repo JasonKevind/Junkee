92facab468a00c197b457e7d541a62eb94717868
Jason Kevin <jasonkevinrn@gmail.com>
2023-11-04 17:59:41 +0530

    Updated

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -29,36 +29,6 @@ export const Schedule=()=>{
         }
         return true;
     }
-    const Recap=()=>{
-        try{
-            const ll=new RecaptchaVerifier("recaptcha-container",{},auth);
-            ll.render();
-            signInWithPhoneNumber(auth,"+91"+contact,ll).then(con=>{
-                const code=prompt("Enter OTP within 1 minute","");
-                con.confirm(code).then(async(r)=>{
-                    const q=query(collection(db,"clients"),where("contact","==",contact));
-                     const ans=await getDocs(q);
-                     if(ans.docs.length){
-                        document.getElementById("globname").innerText=ans.docs[0].data().name;
-                        document.getElementById("globcon").innerText=ans.docs[0].data().contact;
-                        nav("/Admin",{state:{name:ans.docs[0].data().name,contact}});
-                     }else{
-                        nav("/Reg",{state:{number:contact}});
-                     }
-                }).catch(error=>
-                    {alert("Entered incorrect OTP, you can try again.");setContact("");window.location.reload();})
-            }).catch((error)=>{
-                alert("You might have placed more than 8 orders with this number for today/ Try again in sometime/ Try with another number OR you might have disabled pop-ups in your browser(Enable them)"
-                );
-            setContact("");    
-            window.location.reload();
-        })
-        }catch{
-            setContact("");
-            alert("Please try again or contact number entered might be wrong(Register again with correct number) or you might have disabled pop-ups in browser(Enable them)")}
-            window.location.reload();
-        }
-   
     return (
 <div id="sch" style={{display:'flex',justifyContent:'space-evenly',minHeight:'30vh',flexDirection:'column',maxHeight:'49vh'}} >
     <div style={{flexGrow:0.1}}>
@@ -97,7 +67,6 @@ export const Schedule=()=>{
            }
         
         }}
-    
            >NEXT</button>
         </div>)
     }
